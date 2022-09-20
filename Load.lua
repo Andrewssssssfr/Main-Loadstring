62212f90a0149f6df7e4974de9077fbf7e082efa
Andrewssssssfr <105506928+Andrewssssssfr@users.noreply.github.com>
2022-09-20 18:03:59 +0300

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -1,9 +1,1 @@
-if game.PlaceId == 286090429 then
-	if KRNL_LOADED or syn
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Andrewssssssfr/Niksploit-Hub/main/Main.lua",true))()
-	else
-		game.Players.LocalPlayer:Kick("Your Exploit Is Not Supported! Use Krnl Or Synapse, JJSPLOIT (joke)!")
-	end
-else
-	game.Players.LocalPlayer:Kick("Wrong place sir")
-end
+loadstring(game:HttpGet("https://pastebin.com/raw/TFyX1vtB",true))()
